4528d1858333aac9a2a129000d467c6fd3ade78f
Craig Johnson <crajohns@gmail.com>
2023-03-23 07:55:51 -0500

    Update provider.go

--- provider.go
+++ provider.go
@@ -5,7 +5,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/forwardnetworks/terraform-provider-forwardnetworks/forwardnetworks"
+	"terraform-provider-forwardnetworks/forwardnetworks"
 )
 
 func Provider() *schema.Provider {
