41605c8b668915097782bc06bf00bd2ea965ba97
Craig Johnson <craigjohnson@forwardnetworks.com>
2023-03-22 19:20:08 -0500

    client

--- forwardnetworks/client.go
+++ forwardnetworks/client.go
@@ -21,5 +21,40 @@ func NewForwardNetworksClient(username, password, baseURL string) *ForwardNetwor
 	}
 }
 
-func (c *ForwardNetworksClient) GetVersion() (string,
+func (c *ForwardNetworksClient) GetVersion() (string, error) {
+	url := fmt.Sprintf("%s/api/version", c.BaseURL)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.SetBasicAuth(c.Username, c.Password)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("API request failed with status code %d", resp.StatusCode)
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var versionData struct {
+		Version string `json:"version"`
+	}
+	err = json.Unmarshal(bodyBytes, &versionData)
+	if err != nil {
+		return "", err
+	}
+
+	return versionData.Version, nil
+}
 
